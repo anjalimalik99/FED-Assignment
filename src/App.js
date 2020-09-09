412aa965b042e9519679bcee96153113fdce5df6
Amit Kumar <amit.k7@infoedge.com>
2020-09-09 20:28:07 +0530

    master replicated...............

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import Login from './components/Login'
-import './App.css';
-import Register from './components/Register';
-import Services from './components/Services'
-import {Switch , Route, Redirect, BrowserRouter as Router} from 'react-router-dom'
-import Routers from './components/Routers'
- 
+import React from "react";
+import logo from "./logo.svg";
+import Login from "./components/Login";
+import "./App.css";
+import Register from "./components/Register";
+import Services from "./components/Services";
+import {
+  Switch,
+  Route,
+  Redirect,
+  BrowserRouter as Router,
+} from "react-router-dom";
+import Routers from "./components/Routers";
+
 function App() {
   return (
     <div className="App">
-    <Router>
-      <Switch>
-                <Route exact path="/" component={Login}></Route>
-                <Route exact path="/register" component={Register}></Route>
-                <Route exact path="/services" component={Services}></Route>
-                <Redirect to="/"></Redirect>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Login}></Route>
+          <Route exact path="/register" component={Register}></Route>
+          <Route exact path="/services" component={Services}></Route>
+          <Redirect to="/"></Redirect>
         </Switch>
       </Router>
     </div>
@@ -23,3 +28,12 @@ function App() {
 }
 
 export default App;
+
+// marvelapp: https://marvelapp.com/prototype/4efb724/screens
+
+// - [Buy online with hover text](https://marvelapp.com/prototype/4efb724/screen/65992957)
+// - [Cart View](https://marvelapp.com/prototype/4efb724/screen/62078699)
+// - [Login](https://marvelapp.com/prototype/4efb724/screen/62078703)
+// - [Registration Page](https://marvelapp.com/prototype/4efb724/screen/62078714)
+// - [Make Payment - Account Created Successfully](https://marvelapp.com/prototype/4efb724/screen/62078709)
+// - [Make Pending Payment](https://marvelapp.com/prototype/4efb724/screen/62078710)
